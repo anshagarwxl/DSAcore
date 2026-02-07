d54246c198a342c6579a85f13deca77c32094051
Ansh Agarwal <agansh06@gmail.com>
2026-02-07 08:13:05 +0530

    Time: 15 ms (20.62%), Space: 26.3 MB (15.01%) - LeetHub

--- LeetCode/Medium/1283-find-the-smallest-divisor-given-a-threshold/1283-find-the-smallest-divisor-given-a-threshold.cpp
+++ LeetCode/Medium/1283-find-the-smallest-divisor-given-a-threshold/1283-find-the-smallest-divisor-given-a-threshold.cpp
@@ -1,26 +1,28 @@
 class Solution {
 public:
-    int sumByD(vector<int> &arr, int div){
-        int sum=0;
-        int n = arr.size();
-        for(int i =0;i<arr.size();i++){
-            sum = sum+ ceil((double)(arr[i])/(double)(div)); //ceil returns the higher value, if 1.25 -> 2 
-        }
-        return sum;
+   int sumByD(vector<int>&arr, int div){
+    int sum = 0;
+    int n =arr.size();
+    for(int i =0; i<n;i++){
+        sum+=ceil((double)(arr[i])/(double)(div));
     }
-    int smallestDivisor(vector<int>& arr, int limit) {
-        // if(arr.size()>limit) return -1;
-        int low =1;
-        int high = *max_element(arr.begin(),arr.end()); // c++ stl func to give max element in a range
-        while(low<=high){
-            int mid = (low+high)/2;
-            if(sumByD(arr,mid)<=limit){
-                high = mid-1;
-            }
-            else{
-                low=mid+1;
-            }
+    return sum;
+   }
+   int smallestDivisor(vector<int>&arr, int limit){
+    int n = arr.size();
+    if(n>limit)
+    return -1;
+    int low =1;
+    int high = *max_element(arr.begin(),arr.end());
+    while(low<=high){
+        int mid = (low+high)/2;
+        if(sumByD(arr,mid)<=limit){
+            high=mid-1;
         }
-        return low;
+        else{
+            low=mid+1;
+        }
     }
+    return low;
+   }
 };
