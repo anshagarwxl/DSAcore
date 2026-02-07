e3cb06b4a1094c4da782391ee542e0e2c4a57617
Ansh Agarwal <agansh06@gmail.com>
2026-02-07 07:48:11 +0530

    Time: 11 ms (36.35%), Space: 23.1 MB (12.07%) - LeetHub

--- LeetCode/Medium/0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ LeetCode/Medium/0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -1,32 +1,24 @@
 class Solution {
 public:
-    int findMax(vector<int>& v){
-        int m = INT_MIN;
-        for(int i = 0; i < v.size(); i++){
-            m = max(m, v[i]);
+  long long totalHours(vector<int>&v, int k){
+    long long t =0;
+    for(int i=0;i<v.size();i++){
+        t+=ceil((double)v[i]/k); // (double) is a must, to avoid integer division, then ceil is of no use
+    }                            // or instead of ceil use: t+=ceil(v[i]+k-1)/k;
+    return t;
+  }
+  int minEatingSpeed(vector<int>&piles, int h){
+    int low =1; 
+    int high = *max_element(piles.begin(),piles.end());//STL function to find largest value
+    while(low<=high){
+        int mid = (low+high)/2;
+        if(totalHours(piles,mid)<=h){
+            high=mid-1;
         }
-        return m;
-    }
-    
-    long long totalHours(vector<int>& v, int k){
-        long long t = 0;
-        for(int i = 0; i < v.size(); i++){
-            t += (v[i] + k - 1) / k;
+        else{
+            low=mid+1;
         }
-        return t;
     }
-    
-    int minEatingSpeed(vector<int>& piles, int h) {
-        int low = 1, high = findMax(piles);
-        
-        while(low <= high){
-            int mid = low + (high - low) / 2;
-            
-            if(totalHours(piles, mid) <= h)
-                high = mid - 1;
-            else
-                low = mid + 1;
-        }
-        return low;
-    }
+    return low;
+  }
 };
