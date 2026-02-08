20faa603e32de9ec8fa52031608f43cd5340630b
Ansh Agarwal <agansh06@gmail.com>
2026-02-08 11:26:12 +0530

    Time: 15 ms (31.88%), Space: 35.1 MB (52.61%) - LeetHub

--- LeetCode/Medium/1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
+++ LeetCode/Medium/1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
-    int findDays(vector<int>&weights, int cap){
+    int findDays(vector<int> &weights, int cap){
         int day =1;
         int load = 0;
-        for(int i =0;i<weights.size();i++){
+        for(int i=0;i<weights.size();i++){
             if(weights[i]+load>cap){
                 day+=1;
                 load=weights[i];
             }
             else{
-                load+= weights[i];
+                load+=weights[i];
             }
         }
         return day;
     }
-    int shipWithinDays(vector<int>& weights, int days) {
+    int shipWithinDays(vector<int>&weights, int d){
         int low = *max_element(weights.begin(),weights.end());
-        int high = accumulate(weights.begin(),weights.end(),0);//sums up elements in a range
+        int high = accumulate(weights.begin(),weights.end(),0);
         while(low<=high){
             int mid = (low+high)/2;
-            int numberOfDays = findDays(weights, mid);
-            if(numberOfDays<=days){
-                high = mid-1;
+            int numberOfDays = findDays(weights,mid);
+            if(numberOfDays<=d){
+                high = mid -1;
             }
             else{
                 low=mid+1;
