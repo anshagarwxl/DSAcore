e9d78aba46842d686ea340333d864ae0c3e834e1
Ansh Agarwal <agansh06@gmail.com>
2026-03-08 12:54:45 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Easy/1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.cpp
+++ LeetCode/Easy/1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.cpp
@@ -1,18 +1,47 @@
 class Solution {
 public:
     bool arrayStringsAreEqual(vector<string>& word1, vector<string>& word2) {
-    string result1 = "";
+        //BRUTE APPROACH
+    // string result1 = "";
 
-    for(string s : word1){
-        result1 += s;
-    }
-    string result2 ="";
-    for(string s : word2){
-        result2+= s;
-    }
+    // for(string s : word1){
+    //     result1 += s;
+    // }
+    // string result2 ="";
+    // for(string s : word2){
+    //     result2+= s;
+    // }
 
-    if(result1==result2)
+    // if(result1==result2)
+    // return true;
+    // return false;
+
+    int m = word1.size();
+    int n = word2.size();
+
+    int w1i =0; int i = 0;
+    int w2i =0; int j = 0;
+
+    while(w1i < m && w2i < n){
+        if(word1[w1i][i] != word2[w2i][j])
+        return false;
+
+        i++;
+        j++;
+
+        if(i==word1[w1i].length()){
+            i=0;
+            w1i++;
+        }
+        if(j==word2[w2i].length()){
+            j=0;
+            w2i++;
+        }
+    }
+    if(w1i==word1.size() && w2i == word2.size())
     return true;
+
     return false;
+
     }
 };
