827a11d52b028e91df71ebe8a6815c0fe43deffb
Ansh Agarwal <agansh06@gmail.com>
2026-03-25 19:50:19 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Easy/1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.cpp
+++ LeetCode/Easy/1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.cpp
@@ -4,3 +4,20 @@ public:
         return n-1;
     }
 };
+
+//logical approach with time complexity : O(log n)
+
+// int numberOfMatch(int n){
+//     int matches = 0;
+//     while(n>1){
+//         if(n%2==0){ //even
+//             matches +=n/2;
+//             n=n/2;
+//         }
+//         else{
+//             matches +=(n-1)/2;
+//             n = (n-1)/2+1;
+//         }
+//     }
+//     return matches;
+// }
