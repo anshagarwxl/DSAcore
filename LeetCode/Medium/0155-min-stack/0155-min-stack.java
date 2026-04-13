f02057f1b0fcea2ccacd7c6aa5595c01178dc2ea
Ansh Agarwal <agansh06@gmail.com>
2026-04-13 21:17:21 +0530

    Time: 4 ms (100%), Space: 46.9 MB (86.66%) - LeetHub

---
+++ LeetCode/Medium/0155-min-stack/0155-min-stack.java
@@ -0,0 +1,48 @@
+class MinStack {
+    public Stack<Long> st;
+    public long mini;
+
+    public MinStack() {
+        st = new Stack<>();
+    }
+    public void push(int value) {
+        long val = value;
+
+        if(st.isEmpty()){
+            mini = val;
+            st.push(val);
+            return;
+        }
+
+        if(val >= mini){
+            st.push(val);
+        } else {
+            st.push(2*val - mini);
+            mini = val;
+        }
+    }
+    public void pop() {
+        if(st.isEmpty()) return;
+
+        long x = st.pop();
+
+        if(x < mini){
+            mini = 2*mini - x;
+        }
+    }
+    public int top() {
+        if(st.isEmpty()) return -1;
+
+        long x = st.peek();
+
+        if(x >= mini){
+            return (int)x;
+        } else {
+            return (int)mini;
+        }
+    }
+    public int getMin() {
+        if(st.isEmpty()) return -1;
+        return (int)mini;
+    }
+}
